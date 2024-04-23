9196d3352f073492d02c06fe84f8225450a35f2a
Leila Y-Farsani <108967493+leilafarsani@users.noreply.github.com>
2024-04-23 17:29:33 +0100

    problem statement

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+Problem Statement:
+
+Write a function that takes a non-negative integer n as input 
+and returns the factorial of n. 
+
+The factorial of a non-negative integer n, denoted as n!, is the product of 
+all positive integers less than or equal to n. 
+
+For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
+*/ 
