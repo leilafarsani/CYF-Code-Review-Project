d7d0bd41880ba8ba4b3435b39939dfb8ae33cfb9
Leila Y-Farsani <108967493+leilafarsani@users.noreply.github.com>
2024-04-23 17:36:07 +0100

    good solution

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ all positive integers less than or equal to n.
 
 For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
 */ 
+
+function calculateFactorial(n) {
+    if (n < 0) {
+        throw new Error("Input must be a non-negative integer.");
+    }
+    
+    let factorial = 1;
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
